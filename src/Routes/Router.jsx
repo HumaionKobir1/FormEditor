8896e748c2353ee4ca9eb46326576c326ef4fc6f
HumaionKobir1 <humaionkobir341@gmail.com>
2023-11-02 10:14:39 +0600

    Project setup done

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div className="text-center">Hello world!</div>,
+    },
+  ]);
+
+  export default router;
